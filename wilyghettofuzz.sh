d6fa2886ea4b2495befcf6a31c5e0551df14f0c7
wily <wily@unre.st>
2013-05-14 22:01:56 +1000

    Useability and prettyness

--- wilyghettofuzz.sh
+++ wilyghettofuzz.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-# yes, it can be this easy to find a packet of death in a piece of $10 000 enterprise software
-export LC_CTYPE=C; export LANG=C;
-echo WILYGHETTOFUZZ
-echo ==============
+# wilyghettofuzz.sh
+#
+# "yes, it _can_ be this easy to find a packet of death in a piece of $10 000 enterprise software."
 
-target="10.10.10.10"
-declare -a ports; ports=(443);
+export LC_CTYPE=C; export LANG=C;
+
+echo
+echo WILYGHETTOFUZZ v0.0.2
+echo =====================
+
+function usage() {
+        echo "$ ./wilyghettofuzz.sh host ports"
+        echo "$ ./wilyghettofuzz.sh 10.1.1.1 80,443"
+        echo
+}
+
+target="$1"
+declare -a ports; ports=($2);
+
+if [ -z "$target" ] || [ -z "$ports" ]; then
+        usage
+        exit;
+fi
+
 prepend=""; postpend=""
 proto="tcp" # udp or tcp
 declare -i debug; debug=0;
@@ -25,12 +42,14 @@ function ghettofuzz() {
                 done
                 if [ -n "$result_a" ]
                                 then
-                                                echo "We have crashed the server, saving packets and exiting."
+                                                echo "[+] We have crashed the server, saving packets and exiting."
                                                 echo "$a"|xxd -p >> wilyghettofuzz-success.packet.txt
                                                 exit 1
                 fi
 }
  
+echo "[+] Beginning ghettofuzz of $target on ports $ports:"
+
 while true;
                 do ghettofuzz
 done;
